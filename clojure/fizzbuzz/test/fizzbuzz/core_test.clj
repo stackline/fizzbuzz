aa5d00138c7e621bf6469d2234bbd4c1be5dfc98
stackline <kyo10itimanben+noreply@gmail.com>
2018-08-23 00:44:15 +0900

    Add test code of clojure fizzbuzz function

--- clojure/fizzbuzz/test/fizzbuzz/core_test.clj
+++ clojure/fizzbuzz/test/fizzbuzz/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [fizzbuzz.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest fizzbuzz-normal-test
+  (testing "fizzbuzz return"
+    (testing "'fizz' if it is diviible by a multiple of 3."
+      (is (= (fizzbuzz 3) "fizz")))
+    (testing "'buzz' if it is diviible by a multiple of 5."
+      (is (= (fizzbuzz 5) "buzz")))
+    (testing "'fizzbuzz' if it is diviible by a multiple of 3 and a multiple of 5."
+      (is (= (fizzbuzz 15) "fizzbuzz")))))
+
+(deftest fizzbuzz-abnormal-test
+  (testing "fizzbuzz throw class cast exception"
+    (is (thrown? java.lang.ClassCastException (= (fizzbuzz "aaa"))))))
